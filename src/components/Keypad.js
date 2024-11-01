11a553992e2330ceb052221e98fc98c379f9b718
njorogecharles439@gmail.com <njorogecharles439@gmail.com.>
2024-11-01 22:41:20 +0300

    changes

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,22 @@
-// Code Keypad Component Here
+import React from 'react';
 
-function Keypad (){
+// create function for keypad
+function Keypad () {
+    //function to handle the changes
+    function changeHandler () {
+        //on handling the change, print the text
+        console.log('Entering password...');
+        
+    }
     return (
-        <div></div>
-    )
-}
+
+        //pass input type as the password
+        //use onChange to handle the change
+        <input type='password' onChange={changeHandler}/>
+    );
+};
 
 export default Keypad;
+
+
+
